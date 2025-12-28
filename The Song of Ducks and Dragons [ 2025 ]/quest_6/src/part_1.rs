37afb45a6ad00dcc469dcbe821c20fda1abd6ea2
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-28 15:10:15 +0530

    feat: add solution to 2025/quest_6 (part 1)

--- The Song of Ducks and Dragons [ 2025 ]/quest_6/src/part_1.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_6/src/part_1.rs
@@ -1,3 +1,15 @@
-pub fn solve(_input: &str) -> anyhow::Result<()> {
-    todo!()
+pub fn solve(input: &str) -> anyhow::Result<()> {
+    let mut count = 0;
+
+    let mut temp_count = 0;
+    for c in input.chars() {
+        if c == 'A' {
+            temp_count += 1;
+        } else if c == 'a' {
+            count += temp_count;
+        }
+    }
+
+    println!("Answer: {count}");
+    Ok(())
 }
