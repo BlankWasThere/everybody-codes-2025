0fa02d82b5ad5eaea5678a517630aef3a2c0ac77
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-28 15:23:39 +0530

    feat: add solution to 2025/quest_6 (part 2)

--- The Song of Ducks and Dragons [ 2025 ]/quest_6/src/part_2.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_6/src/part_2.rs
@@ -1,3 +1,17 @@
-pub fn solve(_input: &str) -> anyhow::Result<()> {
-    todo!()
+use std::collections::HashMap;
+
+pub fn solve(input: &str) -> anyhow::Result<()> {
+    let mut count = 0;
+    let mut collection = HashMap::new();
+
+    for c in input.chars() {
+        if c.is_uppercase() {
+            collection.entry(c).and_modify(|e| *e += 1).or_insert(1);
+        } else if c.is_lowercase() {
+            count += collection.get(&c.to_ascii_uppercase()).unwrap_or(&0);
+        }
+    }
+
+    println!("Answer: {count}");
+    Ok(())
 }
