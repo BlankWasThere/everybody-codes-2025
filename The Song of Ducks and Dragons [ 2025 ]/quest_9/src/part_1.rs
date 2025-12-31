1b66103d0438e8a1c0b6c7cf795b4ee726928023
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-31 22:22:55 +0530

    feat: add solution to 2025/quest_9 (part 1)

--- The Song of Ducks and Dragons [ 2025 ]/quest_9/src/part_1.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_9/src/part_1.rs
@@ -1,3 +1,63 @@
-pub fn solve(_input: &str) -> anyhow::Result<()> {
-    todo!()
+use std::ptr;
+
+use anyhow::{anyhow, ensure};
+
+pub fn parse_input(input: &str) -> anyhow::Result<Vec<Vec<char>>> {
+    input
+        .trim()
+        .lines()
+        .filter_map(|s| {
+            let s = s.trim();
+            if !s.is_empty() { Some(s) } else { None }
+        })
+        .map(|s| {
+            let (_, t) = s.split_once(':').ok_or(anyhow!("Invalid line `{s}`"))?;
+            Ok(t.chars().collect::<Vec<_>>())
+        })
+        .collect::<anyhow::Result<Vec<_>>>()
+}
+
+pub fn solve(input: &str) -> anyhow::Result<()> {
+    let dna_list = parse_input(input)?;
+    let mut product = 0;
+
+    ensure!(
+        dna_list.len() == 3,
+        "Expected 3 DNA, found {}",
+        dna_list.len()
+    );
+
+    'outer: for curr in &dna_list {
+        let mut first_count = 0;
+        let mut second_count = 0;
+
+        let mut others = dna_list.iter().filter(|&e| !ptr::eq(curr, e));
+
+        for ((&a, &b), &c) in curr
+            .iter()
+            .zip(others.next().unwrap())
+            .zip(others.next().unwrap())
+        {
+            let mut matched = false;
+            if a == b {
+                first_count += 1;
+                matched = true;
+            }
+
+            if a == c {
+                second_count += 1;
+                matched = true;
+            }
+
+            if !matched {
+                continue 'outer;
+            }
+        }
+
+        product = first_count * second_count;
+    }
+
+    println!("Answer: {product}");
+
+    Ok(())
 }
